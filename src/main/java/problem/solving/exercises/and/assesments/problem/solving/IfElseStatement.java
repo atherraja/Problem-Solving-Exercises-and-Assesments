a7ac2065002ffaf189a2eeb66b49334960863411
mather <mather@nisum.com>
2024-08-09 16:42:05 +0500

    If Statement IfElse Statement switch case statements

---
+++ src/main/java/problem/solving/exercises/and/assesments/problem/solving/IfElseStatement.java
@@ -0,0 +1,13 @@
+package problem.solving.exercises.and.assesments.problem.solving;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int time = 20;
+        if(time < 18) {
+            System.out.println("Good Day");
+        }
+        else{
+            System.out.println("Good Evening");
+        }
+    }
+}
---
+++ src/main/java/problem/solving/exercises/and/assesments/problem/solving/IfStatement.java
@@ -0,0 +1,9 @@
+package problem.solving.exercises.and.assesments.problem.solving;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        if (50 > 49){
+            System.out.println("50 is greater than 49");
+        }
+    }
+}
