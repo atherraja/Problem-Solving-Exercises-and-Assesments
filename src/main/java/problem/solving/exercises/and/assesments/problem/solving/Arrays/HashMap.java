77a153460fd96c664b6f2fc69f42a00247cfc977
mather <mather@nisum.com>
2024-08-13 11:04:40 +0500

    Problem Solving & Arrays

---
+++ src/main/java/problem/solving/exercises/and/assesments/problem/solving/Arrays/HashMap.java
@@ -0,0 +1,17 @@
+package problem.solving.exercises.and.assesments.problem.solving.Arrays;
+
+public class HashMap {
+    public static void main(String[] args) {
+        // Create a HashMap object called capitalCities
+        java.util.HashMap<String, String> capitalCities = new java.util.HashMap<String, String>();
+
+        // Add keys and values (Country, City)
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway","Oslo");
+        capitalCities.put("USA","Washington");
+        System.out.println(capitalCities);
+
+
+    }
+}
---
+++ src/main/java/problem/solving/exercises/and/assesments/problem/solving/Arrays/LinkedList.java
@@ -0,0 +1,16 @@
+package problem.solving.exercises.and.assesments.problem.solving.Arrays;
+
+class LinkedList{
+    public static void main(String[] args) {
+        java.util.LinkedList<String> cars = new java.util.LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+
+    }
+
+    }
+
